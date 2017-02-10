ffcd914ec4d900b9c5a65b3a375fc4561512cdb9
Yhugo <yohann.hugo@gmail.com>
2017-02-10 15:26:41 +0100

    Model inscription/connexion

---
+++ src/main/java/model/Identifiant.java
@@ -0,0 +1,26 @@
+package model;
+
+/**
+ * Created by Yhugo on 10/02/2017.
+ */
+public class Identifiant {
+
+    public String login;
+    public String mdp;
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getMdp() {
+        return mdp;
+    }
+
+    public void setMdp(String mdp) {
+        this.mdp = mdp;
+    }
+}
